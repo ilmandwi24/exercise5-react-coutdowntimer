9afb32b3d54e1cf6e932ad780a804ac37bd4d4d4
unknown <ilmandwi20@gmail.com>
2024-06-26 11:27:49 +0700

    selesai

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { IconContext } from "react-icons";
 import { FaInstagram } from "react-icons/fa";
@@ -16,34 +16,65 @@ function App() {
   const inputJam = queryParameters.get("jam");
   const [tanggal,setTanggal] = useState("2024-9-4");
   const [jam,setJam] = useState("00");
-  console.log(inputJam === "");
-  console.log(inputTanggal === "");
-  moment.locale('i')
-  let displayTanggal = moment(tanggal).format('LLL');
-  // console.log(typeof inputTanggal);
-  // console.log(inputTanggal);
-  const date = moment().format("MMMM DD YYYY");
-  const time = moment().format("HH mm ss");
+  const [displayTanggal, setDisplayTanggal] = useState("")
+  
+
+  useEffect(()=>{
+    if(typeof inputTanggal !== 'object')  {
+      if(inputTanggal.length < 8 || inputTanggal.length > 8) return ;
+
+      const tanggalBaru =`${inputTanggal.slice(4)}-${inputTanggal.slice(2,4)}-${inputTanggal.slice(0,2)}`;
+      
+      if(new Date(tanggalBaru).getTime() < new Date().getTime() == true) return ;
+
+      let tanggalSementara = new Date(tanggalBaru).getTime();
+
+      console.log("tanggal default", new Date('2024-06-27').getTime(), "-");
+      console.log("tanggal sementara", new Date(tanggalBaru).getTime(), "-", tanggalBaru);
+      // console.log(moment(tanggalBaru).format('YYYY-MM-DD'));
+      if(typeof inputJam !== 'object') {
+        if(inputJam > 24) {
+          setTanggal(tanggalSementara - 7 * 3600*1000 )
+          setDisplayTanggal(moment(tanggal).format('dddd MMMM Do, YYYY HH:mm:ss'));
+
+            return 
+            
+        };
+        setTanggal(tanggalSementara - 7 * 3600*1000 + inputJam *3600*1000)
+        setDisplayTanggal(moment(tanggal).format('dddd MMMM Do, YYYY HH:mm:ss'));
+
+        return
+      }
+
+      setTanggal(tanggalSementara - 7 * 3600*1000);
+      setDisplayTanggal(moment(tanggal).format('dddd MMMM Do, YYYY HH:mm:ss'));
+
+    }
+    
+
+    
+  })
+  
   return (
     <>
       <section className="h-screen flex items-center justify-center flex-col relative gap-12 px-6 pb-28">
         <h1 className='text-white text-2xl  text-center tracking-widest '>WE'RE LAUNCHING SOON</h1>
-        <div className='w-72  flex'>
+        <div className='flex'>
           <FlipClockCountdown 
-           renderMap={[false, true, true, true]}
-          to={new Date(tanggal)}
+           renderMap={[true, true, true, true]}
+          to={tanggal}
           labelStyle={{ fontSize: 12, fontWeight: 500, textTransform: 'uppercase' }}
           className='flip-clock' />
          
         </div>
-        <p className='text-white'>Date : {displayTanggal}</p>
-        <p className='text-white'>Date : {displayTanggal}</p>
+        <p className='text-white border-[#fa5f86] border-2 p-5'>Launch : <br/> {displayTanggal}</p>
+
         
         <footer className='justify-end  items-end absolute bottom-0 w-full'>
           <div className="h-36  bg-[url('./assets/pattern-hills.svg')] bg-top flex justify-center ">
             <div className='flex mt-16 gap-6'>
-            <p> Today's date is { date } </p>
-            <p> The time is { time } </p>
+            {/* <p> Today's date is { date } </p> */}
+            {/* <p> The time is { time } </p> */}
               {/* <img src={IconFacebook} alt="" className='w-7 h-7 hover:text-black'/>
               <img src={IconPinterest} alt="" className='w-7 h-7 hover:bg-blue-500'/>
               <img src={IconInstagram} alt="" className='w-7 h-7 hover:bg-blue-500'/> */}
